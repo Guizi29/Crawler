ad7739e0dae4d0b51f4a45bff3f94b8241a43aae
Guillaume Cren <117034899+Guizi29@users.noreply.github.com>
2023-06-30 11:09:46 +0000

    Create ex.py

---
+++ ros2_ws/src/crawler/crawler/ex.py
@@ -0,0 +1,28 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class MyPublisher(Node):
+
+    DIRECTION = "NORTH"
+    
+    def __init__(self):
+        super().__init__('my_publisher')
+        self.publisher_ = self.create_publisher(String, 'my_topic', 10)
+        self.timer_ = self.create_timer(1.0, self.publish_message)
+
+    def publish_message(self):
+        msg = String()
+        msg.data = self.DIRECTION
+        self.publisher_.publish(msg)
+        self.get_logger().info('Published: "%s"' % msg.data)
+
+def main(args=None):
+    rclpy.init(args=args)
+    publisher = MyPublisher()
+    rclpy.spin(publisher)
+    publisher.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
